ed66b8571120c73eb41d863977d8715b2e702202
lizenan <lizenan1995@foxmail.com>
2020-08-25 16:31:41 +0800

    remove temply

--- Operational-Accuracy/imageCLEF/data/figure_save.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-im = Image.open('p/2008_000133.jpg')
-# im = Image.open('p/2009_002649.jpg')
-# im = Image.open('c/224_0003.jpg')
-# im = Image.open('c/246_0043.jpg')
-im = im.resize((224, 224))
-im.save('opt1.png')
